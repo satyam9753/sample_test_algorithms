31aef2f58c5d58cf8cace3d3c1ad5a181519865f
satyam9753 <satyam.anand9753@gmail.com>
2019-11-14 19:36:03 +0530

    effective changes made

--- 4.4_flickr-score.py
+++ 4.4_flickr-score.py
@@ -1,9 +1,25 @@
 import numpy as np
 import os
+import sys
 import cv2
 import pandas as pd
-import re2
+import re
 import statistics
+import pickle
+
+from PIL import Image
+from tensorflow.keras.layers import Dense, Activation, Flatten, Dropout
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.optimizers import SGD, Adam
+from datagen import DataGenerator as DataGenerator2
+import datagen
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2 
+from tensorflow.keras.callbacks import ModelCheckpoint
+from sklearn.externals import joblib
+from sklearn.model_selection import train_test_split
+import copy
+from sklearn.feature_extraction import image
+
 
 #import keras
 #from keras.models import Sequential
@@ -17,71 +33,78 @@ import statistics
 
 DATADIR = "/home/satyam/Desktop/personal/flickr"
 
-class Dataset():
-
-    j=1
-    faves_score = list()
-
-    for img in os.listdir(DATADIR):
-
-        img_path = DATADIR + '/' + os.listdir(DATADIR)[img]
-        image = cv2.imread(img_path)
-
-        for file_name in img_path:
-            
-            faves = re2.findall("flickr_"+'j' + "_(\d+).jpg", file_name)
-            if not faves: 
-                continue
-
-            j++
-            faves_score.append(float(faves[0]))
-
-
-
-    median = statistics.median(faves_score)
-
-
-
-class Resize_images():
+def Resize_images(image):
  
-    for img in os.listdir(DATADIR): 
-
-        img_path = DATADIR + '/' + os.listdir(DATADIR)[img]
-        image = cv2.imread(img_path)
-        resultant_image = cv2.resize(img, dsize=(720, 1280), interpolation=cv2.INTER_CUBIC)
-
-
-
-class FFS_label():
-    for i in (faves_score):
-
-    	if faves_score[i] > median:
-    		new_img_path = DATADIR + '/'+"H" + os.listdir(DATADIR)[i]                                              
-
-
-    	else:
-    		new_img_path = DATADIR + '/'+"L" + os.listdir(DATADIR)[i]
+    #for img in os.listdir(DATADIR): 
+    #
+    #    img_path = DATADIR + '/' + img
+    #    image = cv2.imread(img_path)
+    #    resultant_image = cv2.resize(img, dsize=(720, 1280), interpolation=cv2.INTER_CUBIC)
+    x=[]
+    for item in range(len(image)):
+            imResize = copy.deepcopy(image[item])
+            x.append(cv2.resize(imResize, (720,1080)))
+            print(x[item])
+    return x
+            
 
 
-class create_patch():
-    
+def Dataset():
+
+    faves_score = list()
+    image=[]
     for img in os.listdir(DATADIR):
+        #print(img)
+        img_path = DATADIR + '/' + img
+        image.append(cv2.imread(img_path))
 
-        img_patch = cv2.imread(new_img_path)
-        patches = image.extract_patches_2d(img_patch, (224, 224), max_patches = 1)
+        #print (os.listdir(DATADIR))
+    for file_name in os.listdir(DATADIR):
+            
+        faves = re.findall(r"_0.(\d+).jpg", file_name)
+        if not faves: 
+          continue
+        #print (faves[0])
+        faves_score.append(float(faves[0]))                
+    median = statistics.median(faves_score)
+    print (median)
+    print(img_path,image[0])
+    k=Resize_images(image)
+    return faves_score,k,median
 
+
+
+
+#def FFS_label():
+#    for i in (faves_score):
+#
+#    	if faves_score[i] > median:
+#    		new_img_path = DATADIR + '/'+"H" + os.listdir(DATADIR)[i]                                              
+#    	else:
+#   		new_img_path = DATADIR + '/'+"L" + os.listdir(DATADIR)[i]
+
+
+def create_patch(faves_score,imgs,median):
+    for i in range(len(faves_score)):
+        patches = image.extract_patches_2d(imgs[i], (224, 224), max_patches=1)[0]
         ##STORE PATCHES IN DIFFERENT DIRECTORY
+        if (faves_score[i] > median):
+            s="high"+str(i)
+        else:
+            s="low"+str(i)
+        cv2.imwrite("/home/satyam/Desktop/personal/hl/"+s + '.jpg',patches)
+    return patches
 
 
-class VGG_19():
+
+##### VGG-19 model to be done #####
+
+def VGG_19():
 
         HEIGHT = 224
         WIDTH = 224
 
         base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(HEIGHT, WIDTH, 3))
-
-
-
 
         def build_finetune_model(base_model, dropout, fc_layers, num_classes):
             for layer in base_model.layers:
@@ -134,4 +157,10 @@ class VGG_19():
         # history = base_model.predict_generator(train_generator, workers=8)
         print(finetune_model.evaluate_generator(test_generator))
 
+        # testing
+        # finetune_model.predict_generator() 
 
+
+if __name__=='__main__':
+    fav, imgs, median = Dataset()
+    a=create_patch(Dataset())
